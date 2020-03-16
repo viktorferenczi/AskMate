a3561d349e44cd42b22a3f4accc11037f3250b87
viktorferenczi <ferencziviktor11@gmail.com>
2020-03-16 16:59:49 +0100

    adding Question and Answer class

---
+++ AskMate/Answer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMate
+{
+    public class Answer
+    {
+        public string Text { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ AskMate/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMate
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public List<Answer> ListOfAnswers { get; set; }
+        public string Image { get; set; }
+    }
+}
