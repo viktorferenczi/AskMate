97c93368142e70644ff68632c2f874a2c600a323
viktorferenczi <ferencziviktor11@gmail.com>
2020-03-16 17:41:57 +0100

    Ask your question page

--- AskMate/Controllers/HomeController.cs
+++ AskMate/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace AskMate.Controllers
             return View();
         }
 
+        public IActionResult QuestionAsking()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- AskMate/Models/Answer.cs
+++ AskMate/Models/Answer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AskMate
+namespace AskMate.Models
 {
     public class Answer
     {
