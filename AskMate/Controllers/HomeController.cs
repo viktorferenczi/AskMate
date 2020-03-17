1731a872fb819233cb5c7a15d185f02eb496d1be
viktorferenczi <ferencziviktor11@gmail.com>
2020-03-17 12:24:10 +0100

    fixing without domain folder

--- AskMate/Controllers/HomeController.cs
+++ AskMate/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace AskMate.Controllers
             return View();
         }
 
+        public IActionResult Question ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
