f0b4bf7755be3f2759fa7afca6e0c801cdd59235
Krisztian <nagy.ace.krisztian@gmail.com>
2020-03-17 12:43:11 +0100

    Domain

--- AskMate/Controllers/HomeController.cs
+++ AskMate/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using AskMate.Domain;
 using AskMate.Models;
 
 namespace AskMate.Controllers
